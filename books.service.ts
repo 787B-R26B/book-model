52d7ce6f69d9015da3d455dd6fb78ef79744cd93
雨宮 <s.tomoyasu.sa22c@gmail.com>
2025-01-27 22:51:42 +0900

    add: Id serch

--- books.service.ts
+++ books.service.ts
@@ -4,6 +4,18 @@ import { Book } from './book.model';
 @Injectable()
 export class BooksService {
   private books: Book[] = [];
+  findAll() {
+      return this.books;
+    }
+
+    findById(id: string): Book {
+    const book = this.books.find((book) => book.id === id);
+    if (!book) {
+        throw new Error('Book With ID ${id} not found');
+      }
+      return book;
+
+      }
 
   create(book: Book){
       this.books.push(book);
